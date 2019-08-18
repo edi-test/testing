5e0b2212f3c0e6d44d7e27af6224a289c4c6dcdf
edi-test <edibarisic@hotmail.com>
2019-08-18 11:16:14 +0000

    edi-lemp-stack.yml

--- app/redis_get.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'redis_init.php';
-
-$content = $redis->get($key_name);
-
-echo "Key $key_name has been set with the value $content!"; 
-echo "\n";
-?>
--- app/redis_init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$redis = new Redis ();
-$redis->connect('redis', 6379);
-
-$key_name = 'edi-key-name';
-$key_value = 'edi_value';
-
-?>
